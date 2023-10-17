5cc498f8f5f06d94c323f46e2c0d295292180fbc
Marco Mercado <marco@stonepawtraining.com>
2023-10-17 15:08:15 -0400

    Made nav automatically close when a link is clicked

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const hamburger = document.getElementById("hamburger");
+const navButtonsLinks = document.querySelectorAll(".link");
+
 const displayNav = () => {
   const navLinks = document.querySelector(".nav__links");
   const navHyperLinks = document.querySelector(".nav__hyperlinks");
@@ -14,8 +16,27 @@ const displayNav = () => {
   nameContainer.style.display =
     nameContainer.style.display === "block" ? "none" : "block";
   nav.style.height = nav.style.height === "100vh" ? "0vh" : "100vh";
-
-  console.log(nav.style.height);
 };
+
+const closeNav = () => {
+  const navLinks = document.querySelector(".nav__links");
+  const navHyperLinks = document.querySelector(".nav__hyperlinks");
+  const imageContainer = document.querySelector(".nav__image-container");
+  const nameContainer = document.querySelector(".nav__name");
+  const nav = document.querySelector(".nav");
+
+  navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+  navHyperLinks.style.display =
+    navHyperLinks.style.display === "block" ? "none" : "block";
+  imageContainer.style.display =
+    imageContainer.style.display === "block" ? "none" : "block";
+  nameContainer.style.display =
+    nameContainer.style.display === "block" ? "none" : "block";
+  nav.style.height = nav.style.height === "100vh" ? "0vh" : "100vh";
+};
+
+navButtonsLinks.forEach((button) => {
+  button.addEventListener("click", closeNav);
+});
 
 hamburger.addEventListener("click", displayNav);
